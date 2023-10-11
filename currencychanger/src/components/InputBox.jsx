7bddc25785ee2a4aebcc000d783e837423889e1d
IbrarKhan <ibrartalabkhan@gmail.com>
2023-10-11 04:19:28 -0700

    create an inputBox component for reuseability

--- currencychanger/src/components/InputBox.jsx
+++ currencychanger/src/components/InputBox.jsx
@@ -1,35 +1,41 @@
-function box({
+function inputArea({
     label,
     amount,
-    onChangeAmount,
-    onChangeCurrency,
-    currencyOptions,
-    selectCurrency
+    onAmountChange,
+    onCurrencyChange,
+    currencyOptions = [],
+    selectCurrency = "usd",
+
 }
-    ){
-return(
-    <div>
-     <div>
-     <label htmlFor="">{label}</label>
-    <input type="number" value={amount}
-    onChange={(e) => onChangeAmount && onChangeAmount(e.target.value)}
-    />
-     </div>
-    <div>
-        <p>Currency Type</p>
-        <select value={selectCurrency}
-        onChange={(e) => onChangeCurrency && onChangeCurrency(e.target.value)}
-        >
-            {currencyOptions.map((option) => (
-                <option key={option} value={option}>
-                    {option}
-                </option>
-            ))}
-        </select>
-    </div>
-    
-    </div>
-)
+) {
+    return (
+        <div>
+            <div>
+                <label htmlFor="">{label}</label>
+                <input 
+                type="number"
+                placeholder="Amount"
+                value={amount}
+                onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
+                />
+            </div>
+            <div>
+                <p>Currency Type</p>
+                <select
+                value={selectCurrency}
+                onChange={(e) => onCurrencyChange && onCurrencyChange(e.target.value)}
+                >
+                    {currencyOptions.map((option) =>(
+                        <option
+                        key={option}
+                        value={option}>
+                        {option}</option>
+                    )
+                    )}
+                </select>
+            </div>
+        </div>
+    )
 }
 
-export default box;
+export default inputArea;
